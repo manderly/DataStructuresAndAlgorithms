d73a2737da4b5663bedb9edac83dfcb76ef1446f
Mandi Grant <mandi018@gmail.com>
2014-09-03 12:48:34 -0700

    Starting Binary search tree files

---
+++ chapter10_binarysearchtree/binarysearchtree.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var BST = function() {
+  this.root = null;
+};
+
+this._Node = function(data, left, right) {
+  this.data = data;
+  this.left = left;
+  this.right = right;
+};
+
+_Node.prototype.show = function() {
+  return this.data;
+};
+
+
+BST.prototype.insert = function(data) {
+  var n = new Node(data, null, null);
+  if (this.root === null) {
+    this.root = n;
+  }
+  else {
+    var current = this.root;
+    var parent;
+    while (true) {
+      parent = current;
+      if (data < current.data) {
+        current = current.left;
+        if (current === null) {
+          parent.left = n;
+          break;
+        }
+      }
+      else {
+        current = current.right;
+        if (current === null) {
+          parent.right = n;
+          break;
+        }
+      }
+    }
+  }
+}
+
+module.exports = BST;
--- chapter6_linkedlists/linked_list_lecture.js
+++ chapter6_linkedlists/linked_list_lecture.js
@@ -1,14 +1,1 @@
-LinkedListStack.prototype.isEmpty = function() {
-  return (this._first === null);
-};
 
-LinkedListStack.prototype.push = function(item) {
-  var oldfirst = this._first;
-  this._first = this._Node(item);
-  this._first.next = oldfirst;
-};
-
-//stack has: push, pop, peek, clear, isempty
-  //linked list has: next, head, insert, remove, new
-    //node: element, next, new
-//you can use an array or a linkedList to implement a stack
