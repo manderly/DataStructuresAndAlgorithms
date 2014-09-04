7625ff5892bd11fe1467c019e07d558c27c4e3f4
Mandi Grant <mandi018@gmail.com>
2014-09-03 21:02:47 -0700

    Updated cities data, working on off by one error where Seattle is missing from the results

--- chapter11_graphs/ch11_q4_map_graph.js
+++ chapter11_graphs/ch11_q4_map_graph.js
@@ -1,15 +1,19 @@
-var Graph = require('../graph');
-var home = new Graph();
+var Graph = require('../lib/graph');
+var home = new Graph(10);
 
-home.addEdge("Seattle", "Shoreline");
-home.addEdge("Seattle", "SoDo");
-home.addEdge("Shoreline", "Bothell");
-home.addEdge("Bothell", "Kirkland");
-home.addEdge("Bothell", "Lynnwood");
-home.addEdge("Kirkland", "Bothell");
-home.addEdge("Kirkland", "Bellevue");
-home.addEdge("Bellevue", "SoDo");
+home.addEdge(0,1); //Seattle to Shoreline
+home.addEdge(0,2); //Seattle to SoDo
+home.addEdge(1,4); //Shoreline to Bothell
+home.addEdge(2,3); //SoDo to Renton
+home.addEdge(4,6); //Bothell to Lynnwood
+home.addEdge(4,5); //Bothell to Kirkland
+home.addEdge(5,4); //Kirkland to Bothell
+home.addEdge(5,7); //Kirkland to Bellevue
+home.addEdge(6,8); //Lynnwood to Everett
+home.addEdge(7,3); //Bellevue to Renton
+home.vertexList = ["Seattle","Shoreline","SoDo","Renton","Bothell","Kirkland","Lynnwood","Bellevue","Everett"];
 home.showGraph();
+home.topSort();
 /*
   Build a graph that models the map of the area where you live.
   Determine the shortest path from a starting vertex to the last vertex.
