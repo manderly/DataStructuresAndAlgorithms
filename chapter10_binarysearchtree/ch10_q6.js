465c1b4a76e76f6cdcee585c31fb4cef5d75f040
Mandi Grant <mandi018@gmail.com>
2014-09-05 12:31:54 -0700

    questions 1 2 3 4 and 6 now working

--- chapter10_binarysearchtree/ch10_q6.js
+++ chapter10_binarysearchtree/ch10_q6.js
@@ -13,7 +13,7 @@ var wordsBST = new BST();
 //create a text file with words in it
 var words = ["quack","quack","woof","meow","quack",
               "hiss","hiss","squeak","woof","woof",
-              "meow","whimper","growl","lol"]; //8 uniques, woof is the most common
+              "meow","whimper","growl","lol","arf"]; //15 total, 8 uniques, woof is the most common
 
 //create a BST with those words
 function createBST(words) {
@@ -25,10 +25,10 @@ function createBST(words) {
 createBST(words);
 
 //1.  Add a function to the BST class that counts the number of nodes in a BST.
-console.log("There are " + wordsBST.countNodes() + " nodes in this binary search tree.");
+console.log("There are " + wordsBST.countNodes(wordsBST.root) + " nodes in this binary search tree.");
 
 //2. Add a function to the BST class that counts the number of edges in a BST.
-console.log("There are " + wordsBST.countEdges() + " edges in this binary search tree.");
+console.log("There are " + wordsBST.countEdges(wordsBST.root) + " edges in this binary search tree.");
 
 //3. Add a max() function to the BST class that finds the maximum value in a BST.
 console.log("The maximum value in this binary search tree is " + wordsBST.max());
